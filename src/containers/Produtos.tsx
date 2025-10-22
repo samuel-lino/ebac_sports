5debbbd5c6decaecbb4f32069489bf1fde9207d0
samuel <samuellino2006@gmail.com>
2025-10-22 16:14:41 -0300

    tarefa finalizada

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,15 +1,13 @@
 import { useSelector } from 'react-redux'
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../service/api'
 
 import * as S from './styles'
 import { RootReducer } from '../store'
 
-type Props = {
-  produtos: ProdutoType[]
-}
-
-const ProdutosComponent = ({ produtos }: Props) => {
+const ProdutosComponent = () => {
+  const { data: produtos, isLoading } = useGetProdutosQuery()
   const favoritos = useSelector((state: RootReducer) => state.favoritos.itens)
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
@@ -18,10 +16,14 @@ const ProdutosComponent = ({ produtos }: Props) => {
     return IdsDosFavoritos.includes(produtoId)
   }
 
+  if (isLoading) {
+    return <h2>Carregando...</h2>
+  }
+
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import carrinhoSlice from './reducers/carrinho'
 import favoritosSlice from './reducers/favorito'
+import api from '../service/api'
 
 export const store = configureStore({
   reducer: {
     carrinho: carrinhoSlice,
-    favoritos: favoritosSlice
-  }
+    favoritos: favoritosSlice,
+    [api.reducerPath]: api.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware)
 })
 
 export type RootReducer = ReturnType<typeof store.getState>
